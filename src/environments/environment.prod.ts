4d055aaa0388cca1a1f5b63243cf5a517bb554f1
Jyotirmoy Mukherjee <64358983+jyotirmoy-mukherjee@users.noreply.github.com>
2021-06-27 21:17:35 +0100

    Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // // cognito_ipool_id: "ap-south-1:71f1e954-1000-4df7-94aa-9c1110cb2981",
-  // aws_region: "ap-southeast-1",
-  // cognito_upool_id: "ap-southeast-1_9drWTxibK",
-  // cognito_user_pool_web_wlient_id: "11b11qpspbfaa1ikiet4lj1ugj",
-  // commodities_api_base_url: "https://ua7k5j21qa.execute-api.ap-southeast-1.amazonaws.com"
+  aws_region: "eu-west-2",
+  cognito_upool_id: "eu-west-2_LBV1tannA",
+  cognito_user_pool_web_client_id: "13j334c38ovp8nmrvof22mpo1c",
+  commodities_api_base_url: "https://zwa12ypk69.execute-api.eu-west-2.amazonaws.com/staging/api",
+  self_host: "/"
 };
