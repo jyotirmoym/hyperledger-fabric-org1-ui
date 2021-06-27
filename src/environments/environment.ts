b2021ab22d846507d9571ff93f744d687b854d19
Jyotirmoy Mukherjee <64358983+jyotirmoy-mukherjee@users.noreply.github.com>
2021-06-27 20:20:28 +0100

    Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,11 @@
 
 export const environment = {
   production: false,
-  // cognito_ipool_id: "ap-south-1:71f1e954-1000-4df7-94aa-9c1110cb2981",
   aws_region: "eu-west-2",
   cognito_upool_id: "eu-west-2_LBV1tannA",
-  // cognito_identity_pool_id: "ap-southeast-1:e519d92f-0ff3-4f57-bb81-b9ef1b2e14b7",
   cognito_user_pool_web_client_id: "13j334c38ovp8nmrvof22mpo1c",
   commodities_api_base_url: "https://zwa12ypk69.execute-api.eu-west-2.amazonaws.com/staging/api",
-  self_host: "http://localhost:4201"
+  self_host: "/"
 };
 
 /*
